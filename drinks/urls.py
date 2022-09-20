74607b9c6ecad6228fa337716ac359c8c51f5d9a
Eriche Gonzales <erichelouise@gmail.com>
2022-09-20 08:41:19 -0400

    created create, put, delete functions

--- drinks/urls.py
+++ drinks/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from drinks import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
+#return as html
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drinks/', views.drink_list)
-
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail)
 ]
+
+#to use an extension and return as json
+urlpatterns = format_suffix_patterns(urlpatterns)
